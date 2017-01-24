b1b0dbcf0322dc91f5907324384095a538d46f97
HarshGrandeur <hrshgpt73@gmail.com>
2017-01-24 17:28:37 +0530

    ad

--- cimpress.py
+++ cimpress.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
-PAT = ''
+PAT = 'EAAalhZC68jJABAGGz2RF9HslYsd4RduDnjZAImzjILEVsV8dDpsq82Wr5pUjdTm8L0mYYtPnnewyuaq6w4RAZAAfWNjolhTKChTqZCCu9B7MhRuNuugIyAYOgzJkNuZCLynM88c143mlhJVZAIihZAqlbif4hHLmZBZA9tXX4f9q02gZDZD'
 
 @app.route('/', methods=['GET'])
 def handle_verification():
